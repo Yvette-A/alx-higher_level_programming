7c1b90ffe1d5ac79b054403d1759332fbf9dc802
Yvette Achieng <achiengyvette@gmail.com>
2023-06-14 02:20:57 -0700

    minor changes to file

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,7 +2,7 @@
 
 def uniq_add(my_list=[]):
     my_set = set(my_list)
-    sum = 0
+    total = 0
     for item in my_set:
-        sum += item
-    return(sum)
+        total += item
+    return(total)
