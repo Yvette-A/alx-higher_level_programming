b624c6411f9054f73cdd3097c1ee45deef7dca36
Yvette Achieng <achiengyvette@gmail.com>
2023-06-27 03:01:51 -0700

    adding new file

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""defines a class square"""
+
+
+class Square:
+    """defines a class square and instantiates it
+    wit a private attribute size.
+    """
+    def __init__(self, size):
+        """instantiates a square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+        """Return the current square area."""
+        return (self.__size * self.__size)
