2c5dbbf74b6b88f6a8084894fb329ded2808d3d4
Yvette Achieng <achiengyvette@gmail.com>
2023-07-03 04:37:44 -0700

    minor changes to file

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -67,7 +67,8 @@ class Rectangle:
             return ("")
         rectangle = []
         for i in range(self.__height):
-            [rectangle.append(str(self.print_symbol))for j in range(self.__width)]
+            [rectangle.append(str(self.print_symbol))for
+             j in range(self.__width)]
             if i != self.__height - 1:
                 rectangle.append("\n")
         return ("".join(rectangle))
