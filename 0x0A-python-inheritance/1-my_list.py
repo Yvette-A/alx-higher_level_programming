f06f30e3c1648b028466f562aea171deb85d86db
Yvette Achieng <achiengyvette@gmail.com>
2023-07-10 02:34:41 -0700

    minor changes to file

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,8 @@
 
 class MyList(list):
     """inherited from the built-in list function"""
+    def __init__(self, list=None):
+        self.list = []
     def print_sorted(self):
         if all(isinstance(item, int) for item in self):
             print(sorted(self))
