b3d6727719505dcdeae6cd2c2f2acaddf6bec5a9
Yvette Achieng <achiengyvette@gmail.com>
2023-07-10 05:20:53 -0700

    modifies file

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,7 +5,7 @@
 class MyInt(int):
     """inhererits from built in int module"""
     def __eq__(self, value):
-        return self != value
+        return self.real != value
 
     def __ne__(self, value):
-        return self == value
+        return self.real == value
