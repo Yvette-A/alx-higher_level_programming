1e279f04ceff0f654ece29c8d554279ebe279289
Yvette Achieng <achiengyvette@gmail.com>
2023-07-11 02:36:12 -0700

    modified file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""defines a function read_file"""
+"""defines a module read_file"""
 
 
 def read_file(filename=""):
-    """reads text file decoded in UTF8 and prints it is stdout"""
+    """reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
