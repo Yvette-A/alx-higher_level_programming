3aa84bb9d60a39cdc0eb33a0850cf5dea3f52475
Yvette Achieng <achiengyvette@gmail.com>
2023-08-17 04:40:08 -0700

    modifies file

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- creates new user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY user_0d_1_pwd
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY IF EXISTS user_0d_1_pwd
