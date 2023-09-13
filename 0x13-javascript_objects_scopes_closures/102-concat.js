060c17063743a4c0c14f760536ca45cff240b67d
Yvette Achieng <achiengyvette@gmail.com>
2023-09-13 03:29:11 -0700

    removed file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const fs = require('files');
-const x = fs.readFileSync(process.argv[2], 'utf8');
-const y = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], x + y);
