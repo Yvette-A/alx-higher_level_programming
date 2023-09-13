17c81bbd2745450b6db19febb2931520f7d190ef
Yvette Achieng <achiengyvette@gmail.com>
2023-09-13 03:17:03 -0700

    minor correction to file

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,8 +4,7 @@ module.exports = class Square extends require('./5-square.js') {
     if (c === undefined) {
       this.print();
     } else {
-      for (let y = 0; y < this.height; y++)
-	    console.log(c.repeat(this.width));
+      for (let y = 0; y < this.height; y++) console.log(c.repeat(this.width));
     }
   }
 };
