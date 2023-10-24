72af93899aa8b692595feb115e1f6a930729fb26
Yvette Achieng <achiengyvette@gmail.com>
2023-10-24 03:59:15 -0700

    correction

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-
 const request = require('request');
 const filmId = process.argv[2];
-const apiUrl = `http://swapi.co/api/films/${filmId}`;
-
+const apiUrl = 'http://swapi.co/api/films/' + filmId;
 request(apiUrl, function (error, response, body) {
 	if (error) {
 		console.log(error);
